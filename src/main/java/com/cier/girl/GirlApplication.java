cc72e8f9f575a1c7c5f58075db45937170df4afc
liuenci <154910381@qq.com>
2018-06-21 20:42:36 +0800

    init

---
+++ src/main/java/com/cier/girl/GirlApplication.java
@@ -0,0 +1,15 @@
+package com.cier.girl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring 启动必须的文件
+ */
+@SpringBootApplication
+public class GirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlApplication.class, args);
+	}
+}
