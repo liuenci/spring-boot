8377e4936b947a5c17e4946b00649d344c42cc83
liuenci <154910381@qq.com>
2018-06-25 20:50:44 +0800

    spring boot 对数据库的操作

---
+++ src/main/java/com/cier/girl/GirlController.java
@@ -0,0 +1,53 @@
+package com.cier.girl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GirlController {
+    @Autowired
+    public GirlRepository girlRepository;
+
+    /**
+     * 查询所有女生接口
+     * @return
+     */
+    @GetMapping(value = "/girls")
+    public List<Girl> getGirlList(){
+        return girlRepository.findAll();
+    }
+    @PostMapping(value = "/girls")
+    public Girl girlAdd(@RequestParam("cupSize") String cupSize,@RequestParam("age") Integer age){
+        Girl girl = new Girl();
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+        girlRepository.save(girl);
+        return girl;
+    }
+
+    @GetMapping(value = "/girls/{id}")
+    public Girl findGirl(@PathVariable("id") Integer id){
+        return girlRepository.findOne(id);
+    }
+
+    @PutMapping(value = "/girls/{id}")
+    public Girl updateGirl(@PathVariable("id") Integer id,@RequestParam("cupSize") String cupSize,@RequestParam("age") Integer age){
+        Girl girl = new Girl();
+        girl.setId(id);
+        girl.setCupSize(cupSize);
+        girl.setAge(age);
+        girlRepository.save(girl);
+        return girl;
+    }
+    @DeleteMapping(value = "/girls/{id}")
+    public void deleteGirl(@PathVariable("id") Integer id){
+        girlRepository.delete(id);
+    }
+
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> findGirlByAge(@PathVariable("age") Integer age){
+        return girlRepository.findByAge(age);
+    }
+}
---
+++ src/main/java/com/cier/girl/GirlRepository.java
@@ -0,0 +1,12 @@
+package com.cier.girl;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GirlRepository extends JpaRepository<Girl,Integer>{
+
+    // 通过年龄查询
+    List<Girl> findByAge(Integer age);
+
+}
