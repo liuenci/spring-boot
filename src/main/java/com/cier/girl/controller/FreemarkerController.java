e7a4565806a9ee9af8188f2acb116ca1c37b2e3c
liuenci <154910381@qq.com>
2018-08-18 11:54:37 +0800

    整合 freemark

---
+++ src/main/java/com/cier/girl/controller/FreemarkerController.java
@@ -0,0 +1,24 @@
+package com.cier.girl.controller;
+
+import com.cier.girl.pojo.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("ftl")
+public class FreemarkerController {
+    @Autowired
+    private Resource resource;
+
+    @RequestMapping("/index")
+    public String index(ModelMap map) {
+        map.addAttribute("resource", resource);
+        return "freemarker/index";
+    }
+
+    @RequestMapping("center")
+    public String center() {
+        return "freemarker/center/center";
+    }
+}
